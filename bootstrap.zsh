17e1fe4d1158f39961756027742b150672e441a2
Eric Martin <e.a.martin1337@gmail.com>
2013-08-18 07:01:47 +0000

    Initial commit

---
+++ bootstrap.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+dir=`dirname $0`
+cd $dir
+
+config=(.gitconfig .global_gitignore .tmux.conf)
+
+for f in $config; do
+    echo "Symbolically linking ~/$f to $dir/$f"
+    ln -s $dir/$f ~/$f
+done
